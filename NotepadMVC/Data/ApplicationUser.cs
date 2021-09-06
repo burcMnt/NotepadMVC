fbc9ea6d66f6669f8cef4d6789d5304a3598937e
burcMnt <burcumantar0@gmail.com>
2021-09-07 02:32:03 +0300

    Add project files.

---
+++ NotepadMVC/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotepadMVC.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime RegistrationTime { get; set; } = DateTime.Now;
+        public List<Note> Notes { get; set; }
+    }
+}
