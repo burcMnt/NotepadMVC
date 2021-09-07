9b98db6c4566c61ca68352e1701f463aeb3d77d5
burcMnt <burcumantar0@gmail.com>
2021-09-07 13:18:29 +0300

    Adding note page

--- NotepadMVC/Controllers/HomeController.cs
+++ NotepadMVC/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NotepadMVC.Data;
 using NotepadMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace NotepadMVC.Controllers
@@ -12,15 +14,26 @@ namespace NotepadMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext db)
         {
             _logger = logger;
+            this.db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            //giriş yapmış kullanıcı yok ise
+            if (!User.Identity.IsAuthenticated)
+            {
+                return View("IndexAnonymous");
+            }
+            //giriş yapmış kullanıcının Id si
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+             
+            return View(db.Notes.Where(x =>x.AuthorId == userId).ToList());
         }
 
         public IActionResult Privacy()
---
+++ NotepadMVC/Controllers/NotesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotepadMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace NotepadMVC.Controllers
+{
+    [Authorize] //giriş yapıldıgını kontrol eder
+    public class NotesController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public NotesController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Yeni()
+        {
+            return View();
+        }
+        [HttpPost,ValidateAntiForgeryToken]
+        public IActionResult Yeni(Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                note.AuthorId = User.FindFirst(ClaimTypes.NameIdentifier).Value; // Kimin giriş yaptıgını söyler ki
+                db.Notes.Add(note);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
