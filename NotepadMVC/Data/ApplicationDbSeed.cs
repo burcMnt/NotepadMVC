15cd27a2e47c6fe7005413e210218eb197b66d31
burcMnt <burcumantar0@gmail.com>
2021-09-09 11:44:17 +0300

    Add admin part

---
+++ NotepadMVC/Data/ApplicationDbSeed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotepadMVC.Data
+{
+    public static class ApplicationDbSeed
+    {
+        public static async Task SeedRolesAndUsers(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            //admin rolü henüz yoksa oluştur
+            if (!await roleManager.RoleExistsAsync("admin"))
+            {
+                await roleManager.CreateAsync(new IdentityRole() { Name = "admin" });
+            }
+
+            //admin@example.com kullanıcı yoksa oluştur ve sonra admin rolünü ata
+            if (!await userManager.Users.AnyAsync(x => x.Email == "admin@example.com"))
+            {
+                ApplicationUser adminUser = new ApplicationUser()
+                {
+                    UserName = "admin@example.com",
+                    Email = "Admin@example.com",
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(adminUser,"P@ssword1");
+                await userManager.AddToRoleAsync(adminUser, "admin");
+            }
+        }
+    }
+}
--- NotepadMVC/Startup.cs
+++ NotepadMVC/Startup.cs
@@ -33,6 +33,7 @@ namespace NotepadMVC
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
